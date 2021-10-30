e9301626885d6d18b47ca00866c6e15691f46b72
Markianov Nikita <markianovnikita@gmail.com>
2021-10-30 17:00:01 +0300

    Added navmesh for navigation AI and Player

---
+++ Assets/Scripts/Game/AI/ChickenController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Rendering;
+
+namespace Game.AI
+{
+	public class ChickenController : MonoBehaviour
+	{
+		[SerializeField] private Animator _animator;
+		[SerializeField] private NavMeshAgent _navMeshAgent;
+		[SerializeField] private Transform _moveTarget;
+
+		[SerializeField] private float _maxSpeed;
+		[SerializeField] private float _minSpeed;
+		
+		private void Start()
+		{
+			_navMeshAgent.speed = Random.Range(_minSpeed, _maxSpeed);
+			_animator.speed = _navMeshAgent.speed / ((_minSpeed + _maxSpeed) / 2f);
+		}
+
+		private void Update()
+		{
+			if (_navMeshAgent.SetDestination(_moveTarget.position))
+			{
+				_navMeshAgent.isStopped = false;
+			}
+			
+			_animator.SetFloat("Speed", _navMeshAgent.velocity.magnitude / _navMeshAgent.speed);
+		}
+	}
+}
